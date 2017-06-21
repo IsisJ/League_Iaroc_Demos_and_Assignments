ed877175cf0a55e2bfb89f843a2a02756a41ac6a
CatcusCat <league@WTS15.local>
2017-06-20 19:01:28 -0700

    robo camp

---
+++ src/Assignments/Assignment2.java
@@ -0,0 +1,101 @@
+package Assignments;
+
+import java.io.IOException;
+
+import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
+import org.jointheleague.ecolban.rpirobot.IRobotInterface;
+import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
+
+public class Assignment2 extends IRobotAdapter {
+	Sonar sonar = new Sonar();
+	
+	public Assignment2(IRobotInterface iRobot) {
+		super(iRobot);
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Try event listner, rev Monday 2030");
+		IRobotInterface base = new SimpleIRobot();
+		Assignment2 rob = new Assignment2(base);
+		rob.setup();
+		while(rob.loop()){}
+		rob.shutDown();
+		
+	}
+
+	
+	
+	private void setup() throws Exception {
+		//SETUP CODE GOES HERE!!!!!
+		
+	}
+	
+	private boolean loop() throws Exception{
+		//LOOP CODE GOES HERE!!!!!
+		readSensors(100);
+		 driveDirect(500,500);
+		int[] lightBumpReadings = getLightBumps();
+        
+         
+         if(lightBumpReadings[0] > 0){
+ 			driveDirect(300, -100);
+ 			sleep(25);
+ 		
+ 		}else if(lightBumpReadings[1] > 0){
+ 			driveDirect(300, -200);
+ 			sleep(25);
+ 		
+ 		}else if(lightBumpReadings[2] > 0){
+ 			driveDirect(-100, -100);
+ 			sleep(100);
+ 			driveDirect(-500,500);
+ 			sleep(250);
+ 		}
+ 		else if(lightBumpReadings[3] > 0){
+ 			driveDirect(-100, -100);
+ 			sleep(100);
+ 			driveDirect(-500,500);
+ 			sleep(250);
+ 		}
+ 		else if(lightBumpReadings[4] > 0){
+ 			driveDirect(-200, 300);
+ 			sleep(25);
+ 		}
+ 		
+ 		else if(lightBumpReadings[5] > 0){
+ 	 			driveDirect(-200, 300);
+ 	 			sleep(25);
+ 		}
+ 	    
+         if(isBumpLeft()){ 
+ 			driveDirect(-500, 500);
+ 			sleep(250);
+ 		}
+         if(isBumpRight()){
+        	 driveDirect(500,-500);
+        	 sleep(250);
+         }
+
+		return true;
+	}
+	
+	//private void readSensors(int i) {
+		// TODO Auto-generated method stub
+		
+	//}
+
+	private void sleep(int amt){
+		try {
+			Thread.sleep(amt);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void shutDown() throws IOException {
+		reset();
+		stop();
+		closeConnection();
+	}
+}
--- src/Assignments/AssignmentCode.java
+++ src/Assignments/AssignmentCode.java
@@ -1,14 +1,16 @@
 package Assignments;
 
 import java.io.IOException;
-
 import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
 import org.jointheleague.ecolban.rpirobot.IRobotInterface;
 import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 
 public class AssignmentCode extends IRobotAdapter {
 	Sonar sonar = new Sonar();
-	
+	Camera cam;
+	int green = 20;
+	int red = 20;
+	int i= 0;
 	public AssignmentCode(IRobotInterface iRobot) {
 		super(iRobot);
 	}
@@ -18,25 +20,75 @@ public class AssignmentCode extends IRobotAdapter {
 		IRobotInterface base = new SimpleIRobot();
 		AssignmentCode rob = new AssignmentCode(base);
 		rob.setup();
-		while(rob.loop()){}
+		while (rob.loop()) {
+		}
 		rob.shutDown();
+
+	}
+
+	private void setup() throws Exception {
+		// SETUP CODE GOES HERE!!!!!
+		cam = new Camera(50, 50);
+		cam.enableBurst();
+		cam.turnOffPreview();
+		cam.setTimeout(2);
 		
 	}
 
-	
-	
-	private void setup() throws Exception {
-		//SETUP CODE GOES HERE!!!!!
-	}
-	
-	private boolean loop() throws Exception{
-		//LOOP CODE GOES HERE!!!!!
+	private boolean loop() throws Exception {
+		// LOOP CODE GOES HERE!!!!!
+		readSensors(100);
+		driveDirect(500, 160);
+		i++;
+		if(i==100){
+		cam.takeRGBPicture();
 		
+		double a = cam.getGreenPercentage(green, false);
+		double b = cam.getRedPercentage(red, false);
+
+		if (cam.getGreenPercentage(green, false) > red) {
+			driveDirect(500, 500);
+
+		}
+		if (cam.getRedPercentage(red, false) > green) {
+			driveDirect(-500, 500);
+			sleep(300);
+		}
+		}
+	
+		int[] lightBumpReadings = getLightBumps();
 		
+
+		if (lightBumpReadings[3] > 0) {
+			driveDirect(-400, 400);
+			sleep(150);
+
+		} else if (lightBumpReadings[4] > 0) {
+			driveDirect(-300, 400);
+			sleep(150);
+
+		} else if (lightBumpReadings[5] > 0) {
+			driveDirect(-200, 400);
+			sleep(150);
+
+		}
+
+		/*
+		 * if (isBumpLeft() || isBumpRight()) { driveDirect(-100, -100);
+		 * sleep(500); driveDirect(-500, 500); sleep(3000); driveDirect(0, 0);
+		 * 
+		 * return false; }
+		 */
+
 		return true;
 	}
-	
-	private void sleep(int amt){
+
+	// private void readSensors(int i) {
+	// TODO Auto-generated method stub
+
+	// }
+
+	private void sleep(int amt) {
 		try {
 			Thread.sleep(amt);
 		} catch (InterruptedException e) {
@@ -44,7 +96,7 @@ public class AssignmentCode extends IRobotAdapter {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void shutDown() throws IOException {
 		reset();
 		stop();
